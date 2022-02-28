d43ff12d370b4717f91391270c75363c18610a46
Scott Mesdjian <smesd001@sledge.cs.ucr.edu>
2022-02-28 11:55:04 -0800

    worked on shm_open()

--- shm.c
+++ shm.c
@@ -29,11 +29,41 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
+  int i;  
+  uint temp_id = 0;
+  int temp_i;
+  uint found = 0; //declaring local variables
+  uint a;
 
-//you write this
-
-
-
+  acquire(&(shm_table.lock)); //1. acquire lock on shm_table
+  for (i = 0; i< 64; i++) { //2. search for page with .id == id
+    if (shm_table.shm_pages[i].id == id) { //for the if
+      found = shm_table.shm_pages[i].id;
+      temp_i = i;
+      a = PGROUNDUP(found);
+      break;
+    }
+    if ((shm_table.shm_pages[i].id == 0) && (temp_id == 0)) { //for the else
+      temp_id == shm_table.shm_pages[i].id;
+      temp_i = i;
+      a = PGROUNDUP(temp_id); 
+    }
+  }
+  if (found != 0) { // if found
+    mappages(myproc()->pgdir, (char*)a, PGSIZE, V2P(shm_table.shm_pages[temp_i].frame), PTE_W|PTE_U);// a. map it to the existing page (using mappages())
+    shm_table.shm_pages[temp_i].refcnt++; // b. increment refnct
+  }
+  else if (temp_id != 0){ //else if we found a page thats available
+    // a. find first avail page (already done in for loop)
+    shm_table.shm_pages[temp_i].frame = kalloc();// b. page.frame = kalloc()
+    shm_table.shm_pages[temp_i].refcnt++; // c. increment the refnct
+    memset(shm_table.shm_pages[temp_i].frame, 0, PGSIZE);// d. Initialize the frame allocated to 0 (use memset())
+    mappages(myproc()->pgdir, (char*)a, PGSIZE, V2P(shm_table.shm_pages[temp_i].frame), PTE_W|PTE_U);// e. map the new page (using mappages())
+  }
+  
+  *pointer = (char*)myproc()->sz;// 4. return the address of the page through the pointer variable [*pointer = (char*)size;]
+  myproc()->sz = ;// 5. update current process size 
+  release(&(shm_table.lock));// 6. release lock
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
